d096db892764eba7da90e7a1703a2f684a99ac3c
afattahi1387 <afattahi1387@gmail.com>
2022-09-21 15:08:41 +0430

    seperated admins dashboard html structure and header and sidebar.

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,9 @@
+<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+    <!-- Navbar Brand-->
+    <a class="navbar-brand ps-3" href="{{ route('home') }}">{{ env('APP_NAME') }}</a>
+    <!-- Sidebar Toggle-->
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+    <!-- Navbar Search-->
+    <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+    </form>
+</nav>
