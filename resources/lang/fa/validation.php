b5e5fe83c6c95d7137e1afa33fe8e045403c6e9b
afattahi1387 <afattahi1387@gmail.com>
2022-09-21 14:46:54 +0430

    added persian validations.

---
+++ resources/lang/fa/validation.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    'required' => 'فیلد :attribute الزامی است.',
+    
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+
+    'attributes' => [
+        'email' => 'ایمیل',
+        'password' => 'رمز عبور'
+    ],
+
+];
