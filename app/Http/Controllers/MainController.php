74b9f1125e4dfe0b67298adc24e0b73a8d48b749
afattahi1387 <afattahi1387@gmail.com>
2022-09-21 17:09:20 +0430

    added set_flash_message and get_flashed_messages to MainController.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,6 +12,22 @@ class MainController extends Controller
         $this->middleware('auth');
     }
 
+    public function get_flashed_messages() {
+        $flashed_messages = [];
+        foreach(session()->all() as $session_name => $session_value) {
+            if(substr($session_name, 0, 6) == 'flash_') {
+                $flashed_messages[substr($session_name, 6)] = $session_value;
+                session()->forget($session_name);
+            }
+        }
+
+        return $flashed_messages;
+    }
+
+    public function set_flash_message($type, $message) {
+        session()->put('flash_' . $type, $message);
+    }
+
     public function home() {
         $lessons_subjects = LessonSubject::orderBy('id', 'DESC')->get();
         if(isset($_GET['edit-lesson-subject']) && !empty($_GET['edit-lesson-subject'])) {
@@ -19,7 +35,7 @@ class MainController extends Controller
         } else {
             $edit_form_subject_name = '';
         }
-        return view('main_views.home', ['lessons_subjects' => $lessons_subjects, 'edit_form_subject_name' => $edit_form_subject_name]);
+        return view('main_views.home', ['lessons_subjects' => $lessons_subjects, 'edit_form_subject_name' => $edit_form_subject_name, 'flashed_messages' => self::get_flashed_messages()]);
     }
 
     public function create_lesson_subject(AddAndEditLessonSubjectRequest $request) {
@@ -27,19 +43,23 @@ class MainController extends Controller
             'subject_name' => $request->subject_name
         ]);
 
+        self::set_flash_message('success', 'موضوع درس شما با موفقیت اضافه شد.');
         return redirect()->route('home');
     }
 
     public function delete_lesson_subject(LessonSubject $subject) {
         $subject->delete();
+        self::set_flash_message('success', 'موضوع درس شما با موفقیت حذف شد.');
         return redirect()->route('home');
     }
 
     public function update_lesson_subject(LessonSubject $subject, AddAndEditLessonSubjectRequest $request) {
+        $old_subject_name = $subject->subject_name;
         $subject->update([
             'subject_name' => $request->subject_name
         ]);
 
+        self::set_flash_message('success', 'موضوع درس مورد نظر شما با نام ' . $old_subject_name . ' با موفقیت ویرایش و تبدیل به ' . $request->subject_name . ' شد.');
         return redirect()->route('home');
     }
 }
