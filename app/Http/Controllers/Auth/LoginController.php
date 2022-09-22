2d3a44f659d3d6a4afb7187b21544630c74af66f
afattahi1387 <afattahi1387@gmail.com>
2022-09-22 16:29:32 +0330

    converted redirecTo after login to /redirect-to-dashboard.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/redirect-to-dashboard';
 
     /**
      * Create a new controller instance.
