25e4114e3ffd9656d382254057b8b3b1f9e3ffaf
afattahi1387 <afattahi1387@gmail.com>
2022-09-22 07:19:03 +0330

    added works table migration.

---
+++ database/migrations/2022_09_22_025727_create_works_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('lesson_subject_id');
+            $table->foreign('lesson_subject_id')->references('id')->on('lesson_subjects')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->boolean('presence')->default(0);
+            $table->boolean('home_work')->default(0);
+            $table->boolean('class_work')->default(0);
+            $table->integer('score');
+            $table->text('description')->nullable();
+            $table->integer('day');
+            $table->integer('month');
+            $table->integer('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
