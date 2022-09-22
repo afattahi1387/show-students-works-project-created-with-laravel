d4afaf6dcef3f3d1c26f96d5e5ebb1039725aafa
afattahi1387 <afattahi1387@gmail.com>
2022-09-22 07:19:42 +0330

    added works method for get student works.

--- app/User.php
+++ app/User.php
@@ -26,4 +26,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function works() {
+        return $this->hasMany(Works::class);
+    }
 }
