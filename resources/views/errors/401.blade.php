512c2b41ff5729a7428e4d175d212b7d2b6a9d60
afattahi1387 <afattahi1387@gmail.com>
2022-09-23 19:26:47 +0330

    added 401, 404 and 500 errors page to project.

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fa">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>ارور 401 - شما به این صفحه دسترسی ندارید!</title>
+        <link rel="icon" href="{{ asset('images/icons/error.jpg') }}">
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div id="layoutError">
+            <div id="layoutError_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-6">
+                                <div class="text-center mt-4">
+                                    <h1 class="display-1">401</h1>
+                                    <p class="lead" style="direction: rtl;">شما به این صفحه دسترسی ندارید!</p>
+                                    <a href="{{ route('home') }}" style="text-decoration: none;">
+                                        <i class="fas fa-arrow-left me-1"></i>
+                                        بازگشت به صفحه اصلی
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/scripts.js') }}"></script>
+    </body>
+</html>
