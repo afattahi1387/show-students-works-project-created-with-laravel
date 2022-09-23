7d0070854e60127048f28b28804d254892d8bc9e
afattahi1387 <afattahi1387@gmail.com>
2022-09-23 17:44:09 +0330

    showing user image in site header.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,6 +4,14 @@
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
-    <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-    </form>
+    <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0"></form>
+    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+        <li class="nav-item dropdown">
+            @if(empty(auth()->user()->image))
+                <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+            @else
+                <img src="{{ asset('images/students_images/' . auth()->user()->image) }}" style="width: 45px; height: 45px;">
+            @endif
+        </li>
+    </ul>
 </nav>
